c7c6f3d60db90e99dcfc75161e2431f33393224b
KursNim <kursnim@gmail.com>
2020-08-29 14:34:56 +0900

    Updates

---
+++ precache-manifest.087e387d5657bf32870c4d5de4faef95.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2554af05699af827df295d1e29659397",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "1ed19eba6709a7a7d6d8",
+    "url": "/react-deploy/static/css/main.b312a01f.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/react-deploy/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ed19eba6709a7a7d6d8",
+    "url": "/react-deploy/static/js/main.45516d50.chunk.js"
+  },
+  {
+    "revision": "9a6b4006b58ad85b50f6",
+    "url": "/react-deploy/static/js/runtime-main.3e11c617.js"
+  },
+  {
+    "revision": "6ffbf619223cd12f5aa95a205231b2c6",
+    "url": "/react-deploy/static/media/kks.6ffbf619.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
